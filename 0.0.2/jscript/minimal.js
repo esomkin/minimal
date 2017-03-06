27bfd10fdeae276a74f252f9b6d4b1cf8e9b93e9
Evgeniy <esomkin@gmail.com>
2017-03-06 14:06:12 +0300

    Merge conflicts remove

--- 0.0.2/jscript/minimal.js
+++ 0.0.2/jscript/minimal.js
@@ -52,25 +52,7 @@ THE SOFTWARE.
 
 	var MessageFloat = function () {
 
-<<<<<<< HEAD
-		var _default = {
-
-			position: 'fixed',
-			type: 'danger',
-			placement: {
-
-				from: 'bottom',
-				align: 'right',
-			},
-			offset: 0,
-			spacing: 0,
-			delay: 5000,
-			timer: 1000,
-			allow_dismiss: true,
-		};
-=======
 	};
->>>>>>> origin/master
 
 	MessageFloat._default = {
 
@@ -231,18 +213,7 @@ THE SOFTWARE.
 				$message.removeClass('hidden');
 			}
 
-<<<<<<< HEAD
-				var $parent = $message.parent();
-
-				if (!$parent.hasClass('has-error')) {
-
-					$parent.addClass('has-error');
-				}
-
-				if (!this.messages) {
-=======
 			var $parent = $message.parent();
->>>>>>> origin/master
 
 			if (!$parent.hasClass('has-error')) {
 
@@ -263,17 +234,8 @@ THE SOFTWARE.
 				var $message = this.messages.shift();
 				$message.addClass('hidden');
 
-<<<<<<< HEAD
-					var $message = this.messages.shift();
-					$message.addClass('hidden');
-
-					var $parent = $message.parent();
-					$parent.removeClass('has-error');
-				}
-=======
 				var $parent = $message.parent();
 				$parent.removeClass('has-error');
->>>>>>> origin/master
 			}
 		}
 	};
@@ -741,15 +703,8 @@ THE SOFTWARE.
 
 	Minimal.Message = MessageFactory;
 	Minimal.Loading = Loading;
-<<<<<<< HEAD
 	Minimal.Locale = Locale;
 	Minimal.Url = Url;
-=======
-<<<<<<< HEAD
-=======
-	Minimal.Locale 	= Locale;
->>>>>>> origin/master
->>>>>>> origin/master
 
 	return Minimal;
 }));
